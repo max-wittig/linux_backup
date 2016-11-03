eabb3fcca5f0cf3c1fc717b34c9e8012297f14e0
Max Wittig <max.wittig95@gmail.com>
2016-11-03 19:52:24 +0100

    added output, how many files were deleted

--- cleaner.py
+++ cleaner.py
@@ -12,12 +12,15 @@ class Cleaner:
         self.clean_drive()
 
     def clean_file_system(self):
+        i = 0
         for filename in os.listdir(self.path):
             file = os.path.join(self.path, filename)
             creation_time = int(os.path.getctime(file))
             current_time = int(time.time())
             if current_time - creation_time > self.clean_time:
                 os.remove(file)
+                i += 1
+        print(str(i) + " old files cleaned")
 
     def clean_drive(self):
         pass
